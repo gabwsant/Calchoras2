ea679d5a7e1b3e533ee5c2e1e3d94eaf4abc3ae6
Gabriel Barbosa Santos <gabrielsantos.gbs@outlook.com>
2025-08-09 18:52:03 -0300

    first commit

---
+++ src/main/java/com/calchoras/CalchorasApplication.java
@@ -0,0 +1,13 @@
+package com.calchoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalchorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalchorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calchoras/CalchorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calchoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalchorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
